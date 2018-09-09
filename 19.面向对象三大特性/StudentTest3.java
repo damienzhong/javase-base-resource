751130c80aa9f39efe7c74c4f52d3084e3bb87de
damienzhong <damienzhong@foxmail.com>
2018-09-09 21:47:18 +0800

    创建对象过程

--- 19.面向对象三大特性/StudentTest3.java
+++ 19.面向对象三大特性/StudentTest3.java
@@ -20,12 +20,21 @@
 		目前的代码是为了练习的一种标准格式
 		给成员变量辅助有两种方式，可以只写一种
 		如果不单独获取数据，可以不写getXxx()方法
+		
+	类的初始化过程：
+		加载Student.class文件进内存
+		在栈内存中为s开辟空间
+		在堆内存中为学生对象开辟空间
+		对学生对象的成员变量进行默认初始化
+		对学生对象的成员变量进行显示初始化
+		通过构造方法对学生对象的成员变量赋值
+		学生对象初始化完毕，把对象地址赋值给s
 */
 class Student{
 	//姓名
-	private String name;
+	private String name = "呆萌钟";
 	//年龄
-	private int age;
+	private int age = 23;
 	
 	public Student(){}
 	
@@ -59,8 +68,11 @@ class Student{
 public class StudentTest3{
 	public static void main(String[] args){
 		//方式1给成员变量赋值
-		Student s1 = new Student();
-		s1.setName("呆萌钟");
+		Student s = new Student("啊呆老师",24);
+		s1.show();
+		
+		
+		/*s1.setName("呆萌钟");
 		s1.setAge(24);
 		//输出值
 		System.out.println(s1.getName()+"---"+s1.getAge());
@@ -70,6 +82,6 @@ public class StudentTest3{
 		//方式2给成员变量赋值
 		Student s2 = new Student("呆萌钟2",18);
 		System.out.println(s2.getName()+"---"+s2.getAge());
-		s2.show();
+		s2.show();*/
 	}
 }
