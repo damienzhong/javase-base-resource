c02f5f882bf8f18e6890609e79d5e4ad8de3fb0d
damienzhong <damienzhong@foxmail.com>
2018-10-03 22:29:54 +0800

    多态代码体现

--- 19.面向对象三大特性/多态/DuoTaiDemo.java
+++ 19.面向对象三大特性/多态/DuoTaiDemo.java
@@ -6,11 +6,32 @@
 		
 		猫 m = new 猫();
 		动物 d = new 猫();
-		动物 d = new 动物();
 		
 	多态的前提：
 		A：要有继承关系
 		B：要有方法重写
+			其实没有方法重写也是可以的，但是如果没用这个就没有意义
 		C：要有父类引用指向子类对象
-			new 子();
+			父 f = new 子();
+			
+		用代码体现一下多态
 */
+class Fu{
+	public void show(){
+		System.out.println("show Fu");
+	}
+}
+
+class Zi extends Fu{
+	public void show(){
+		System.out.println("show Zi");
+	}
+}
+
+public class DuoTaiDemo{
+	public static void main(){
+		//要有父类引用指向子类对象
+		//父 f = new 子();
+		Fu f = new Zi();
+	}
+}
