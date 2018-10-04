951569d1fbba4ce4d8e76af229f8d3b497a3470a
damienzhong <damienzhong@foxmail.com>
2018-10-04 23:06:29 +0800

    多态成员访问特点

--- 19.面向对象三大特性/多态/DuoTaiDemo.java
+++ 19.面向对象三大特性/多态/DuoTaiDemo.java
@@ -11,16 +11,44 @@
 			其实没有也是可以的，但是如果没用这个就没有意义。
 		C：要有父类引用指向子类对象。
 			父 f = new Zi();
+			
+	多态中的成员访问特点：
+		A：成员变量
+			编译看左边，运行看左边
+		B：构造方法
+			创建子类对象的时候，访问父类的构造方法，对父类的数据进行初始化。
+		C：成员方法
+			编译看左边，运行看右边
+		D：静态方法
+			编译看左边，运行看左边
+			(静态和类相关，算不上重写，所以访问还是左边的)
+			
+		由于成员方法存在方法重写，所以它运行看右边。
 */
 class Fu{
+	public int num = 100;
 	public void show(){
 		System.out.println("show Fu");
+	}
+	
+	public static void function(){
+		System.out.println("function Fu");
 	}
 }
 
 class Zi extends Fu{
+	public int num = 1000;
+	public int num2 = 200;
 	public void show(){
 		System.out.println("show Zi");
+	}
+	
+	public void method(){
+		System.out.println("method Zi");
+	}
+	
+	public static void function(){
+		System.out.println("function Zi");
 	}
 }
 
@@ -29,5 +57,12 @@ public class DuoTaiDemo{
 		//要有父类引用指向子类对象。
 		//父 f = new Zi();
 		Fu f = new Zi();
+		System.out.println(f.num);
+		//找不到符号
+		//System.out.println(f.num2);
+		f.show();
+		//找不到符号
+		//f.method();
+		f.function();
 	}
 }
