0b09d0c974c0cc0f12669a5557b62318213e6609
damienzhong <damienzhong@foxmail.com>
2018-10-08 23:13:00 +0800

    抽象类成员特点

---
+++ 19.面向对象三大特性/多态/AbstractDemo3.java
@@ -0,0 +1,15 @@
+/*
+	一个类如果没有抽象方法，可不可以定义为抽象类？如果可以，有什么意义？
+	
+	
+	
+	抽象类不能和哪些关键字共存
+		private 冲突
+		final 冲突
+		static 无意义
+*/
+public class AbstractDemo3{
+	public static void main(String[] args){
+		
+	}
+}
