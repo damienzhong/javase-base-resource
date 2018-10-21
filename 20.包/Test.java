6205c619346258ca31b6dd377bcbfce64391c24f
damienzhong <damienzhong@foxmail.com>
2018-10-21 22:24:09 +0800

    导包

--- 20.包/Test.java
+++ 20.包/Test.java
@@ -1,12 +1,28 @@
 /*
 	测试类
+	
+	导包：
+		格式：import 包名;
+			这种方式导入是到类的名称
+		注意：我们用谁就导谁
+	面试题：
+		package,import,class有没有顺序关系？
+		有！
+		package>import>class
+		
+		package:只能有一个
+		import:可以有多个
+		class:可以有多个，以后我们建议是一个
 */
 package cn.damien;
 
+import com.damien.Demo;
+
 class Test{
 	public static void main(String[] args){
-		//Demo d = new Demo();
-		com.damien.Demo d = new com.damien.Demo();
+		Demo d = new Demo();
+		System.out.println(d.sum(10,20));
+		/*com.damien.Demo d = new com.damien.Demo();
 		System.out.println(d.sum(10,20));
 		
 		com.damien.Demo d2 = new com.damien.Demo();
@@ -17,6 +33,7 @@ class Test{
 		
 		com.damien.Demo d4 = new com.damien.Demo();
 		System.out.println(d4.sum(10,20));
+		*/
 	}
 }
 
