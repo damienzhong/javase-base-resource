95d77d46431d30da1d959ed2afc2cc393eb1bc4c
damienzhong <damienzhong@foxmail.com>
2018-10-21 11:24:01 +0800

    包的定义及其注意事项

--- 20.包/PackageDemo.java
+++ 20.包/PackageDemo.java
@@ -34,4 +34,17 @@
 				DelTeacher
 				UpdateTeacher
 				FindTeacher
+		
+	包的定义：
+		package 包名;
+		多级包用.分开即可
+	注意事项：
+		package语句必须是程序的第一条可执行的代码。
+		package语句在一个Java文件中只能有一个。
+		如果没有package，默认表示无包名
 */
+package com.damien;
+
+public class PackageDemo{
+	
+}
