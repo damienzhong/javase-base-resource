92120086c760d98f3976450e8b3c8d6d4ec4d3ca
damienzhong <damienzhong@foxmail.com>
2018-10-21 11:08:08 +0800

    包的概述

---
+++ 20.包/PackageDemo.java
@@ -0,0 +1,37 @@
+/*
+	包：
+		A：其实就是文件夹
+		B：作用
+			a：把相同的类名放到不同的包中
+			b：对类进行分类管理
+			
+	举例：
+		学生：增加、删除、修改、查询
+		老师：增加、删除、修改、查询
+		。。。。
+		
+		方案1：按照功能分
+			com.damien.add
+				AddStudent
+				AddTeacher
+			com.damien.del
+				DelStudent
+				DelTeacher
+			com.damien.update
+				UpdateStudent
+				UpdateTeacher
+			com.damien.find
+				FindStudent
+				FindTeacher
+		方案2：按照模块分
+			com.damien.student
+				AddStudent
+				DelStudent
+				UpdateStudent
+				FindStudent
+			com.damien.teacher
+				AddTeacher
+				DelTeacher
+				UpdateTeacher
+				FindTeacher
+*/
