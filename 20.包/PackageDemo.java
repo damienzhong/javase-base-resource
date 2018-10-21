99cd707ef6c71f9eea0036ab173723ed2531ff96
damienzhong <damienzhong@foxmail.com>
2018-10-21 12:07:16 +0800

    带包类的编译

--- 20.包/PackageDemo.java
+++ 20.包/PackageDemo.java
@@ -42,9 +42,23 @@
 		package语句必须是程序的第一条可执行的代码。
 		package语句在一个Java文件中只能有一个。
 		如果没有package，默认表示无包名
+	带包的类的编译和运行：
+		手动式：
+			a:javac编译当前类文件
+			b:手动建立包对应的文件夹
+			c:把a步骤的class文件放到b步骤的最终文件夹下
+			d:通过java命令运行
+				需要带包名称的执行。
+				java com.damien.PackageDemo
+		自动式：
+			javac编译的时候带上-d即可
+				java -d . PackageDemo.java
+			通过java命令运行，和手动式一样。
 */
 package com.damien;
 
 public class PackageDemo{
-	
+	public static void main(String[] args){
+		System.out.println("呆萌钟的淘宝男装店：DM超人社区 冬装上线拉，欢迎大家进店选购");
+	}
 }
