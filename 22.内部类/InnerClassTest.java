65c45116cb332f48e0637fdaff4edf43fb54178c
damienzhong <damienzhong@foxmail.com>
2018-10-24 11:43:34 +0800

    成员内部类面试题

---
+++ 22.内部类/InnerClassTest.java
@@ -0,0 +1,22 @@
+/*
+	面试题：
+		要求请填空，分别输出30，20，10
+*/
+class Outer{
+	public int num = 10;
+	class inner{
+		public int num = 20;
+		public void show(){
+			int num = 30;
+			System.out.println(?);
+			System.out.println(?);
+			System.out.println(?);
+		}
+	}
+}
+
+public class InnerClassTest{
+	public static void main(String[] args){
+		
+	}
+}
