cd723560bcab111878c4e072a0f313e2b5312ce6
damienzhong <damienzhong@foxmail.com>
2018-11-12 10:31:23 +0800

    module

---
+++ IDEA/project01/module01/src/com/damien/TaoBaoShop.java
@@ -0,0 +1,8 @@
+package com.damien;
+
+public class TaoBaoShop {
+    public static void main(String[] args) {
+        System.out.println("呆萌在的淘宝男装店铺名：DM超人社区");
+        System.out.println("DM超人社区 欢迎大家的光临！");
+    }
+}
