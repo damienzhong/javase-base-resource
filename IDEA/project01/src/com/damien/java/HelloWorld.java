e39971d60b42e378a8aa811a0549c71c13b88382
damienzhong <damienzhong@foxmail.com>
2018-11-12 23:23:22 +0800

    debug

--- IDEA/project01/src/com/damien/java/HelloWorld.java
+++ IDEA/project01/src/com/damien/java/HelloWorld.java
@@ -6,5 +6,17 @@ public class HelloWorld {
         System.out.println("Hello Word!");
         System.out.println("Hello Demien!");
         System.out.println("Hello IDEA!");
+
+        int a = 1;
+        int b=2;
+        a = b;
+        System.out.println(a);
+        System.out.println(b);
+
+        for(int i=0;i<100;i++){
+            if(i%2==0){
+                System.out.println("呆萌在的淘宝男装店铺名：DM超人社区");
+            }
+        }
     }
 }
