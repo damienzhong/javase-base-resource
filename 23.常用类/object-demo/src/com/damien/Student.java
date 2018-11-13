a0432cd011dc61ebc510c526688cf0d6e3a69e68
damienzhong <damienzhong@foxmail.com>
2018-11-13 22:18:19 +0800

    Object概述

---
+++ 23.常用类/object-demo/src/com/damien/Student.java
@@ -0,0 +1,11 @@
+package com.damien;
+
+/**
+ * @author damienzhong
+ */
+public class Student extends Object{
+
+    public Student(){
+        super();
+    }
+}
---
+++ 23.常用类/object-demo/src/com/damien/StudentTest.java
@@ -0,0 +1,15 @@
+package com.damien;
+
+/**
+ * @author damienzhong
+ */
+
+/**
+ * Object：Object类是类层次结构的根类，每个类都有 Object作为超类
+ * 每个类都直接或者间接继承子Object
+ */
+public class StudentTest {
+    public static void main(String[] args) {
+
+    }
+}
