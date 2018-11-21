59efc7a078f899f3cc6c3c0d431eadbde6fdd44e
damienzhong <damienzhong@foxmail.com>
2018-11-21 23:01:46 +0800

    Object method equals() 优化

---
+++ 23.常用类/object-demo/src/com/damien/day04/Student.java
@@ -0,0 +1,52 @@
+package com.damien.day04;
+
+import java.util.Objects;
+
+/**
+ * @author damienzhong
+ */
+public class Student implements Cloneable{
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
