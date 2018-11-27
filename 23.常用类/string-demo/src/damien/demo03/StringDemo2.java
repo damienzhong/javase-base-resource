62282814b835f1cf0fbe6fa683095f2b0aac3acd
damienzhong <damienzhong@foxmail.com>
2018-11-27 22:54:34 +0800

    String获取相关方法

--- 23.常用类/string-demo/src/damien/demo03/StringDemo2.java
+++ 23.常用类/string-demo/src/damien/demo03/StringDemo2.java
@@ -2,17 +2,51 @@ package damien.demo03;
 
 /**
  * String类的获取功能：
- *  public int length()
- *  public char charAt(int index)
- *  public int indexOf(int ch)
- *  public int indexOf(String str)
- *  public int indexOf(int ch,int fromIndex)
- *  public int indexOf(String str,int fromIndex)
- *  public String substring(int beginIndex)
- *  public String substring(int beginIndex,int endIndex)
+ *  public int length()：获取字符串的长度
+ *  public char charAt(int index)：获取指定索引位置的字符
+ *  public int indexOf(int ch)：返回指定字符在此字符串中第一次出现的索引
+ *      为什么这里是int类型，而不是char类型？
+ *      原因：'a'和97其实都可以代表'a'
+ *  public int indexOf(String str):返回指定字符串在此字符串中第一次出现的索引
+ *  public int indexOf(int ch,int fromIndex)：返回指定字符在此字符串中从指定位置后第一次出现处的索引
+ *  public int indexOf(String str,int fromIndex)：返回指定字符串在此字符串中从指定位置后第一次出现处的索引
+ *  public String substring(int beginIndex)：从指定位置开始截取字符串，默认到末尾
+ *  public String substring(int beginIndex,int endIndex)：从指定位置开始到指定位置结束截取字符串。
  */
 public class StringDemo2 {
     public static void main(String[] args) {
+        //定义一个字符串对象
+        String s = "helloworld";
 
+        //public int length()：获取字符串的长度
+        System.out.println("s.length:"+s.length());
+        System.out.println("-------------------");
+
+        //public char charAt(int index)：获取指定索引位置的字符
+        System.out.println("charAt:"+s.charAt(1));
+        System.out.println("-------------------");
+
+        //public int indexOf(int ch)：返回指定字符在此字符串中第一次出现的索引
+        System.out.println("indexOf:"+s.indexOf('l'));
+        System.out.println("-------------------");
+
+        //public int indexOf(String str):返回指定字符串在此字符串中第一次出现的索引
+        System.out.println("indexOf:"+s.indexOf("owo"));
+        System.out.println("-------------------");
+
+        //public int indexOf(int ch,int fromIndex)：返回指定字符在此字符串中从指定位置后第一次出现处的索引
+        System.out.println("indexOf:"+s.indexOf('l',4));
+        System.out.println("indexOf:"+s.indexOf('k',4));
+        System.out.println("indexOf:"+s.indexOf('l',40));
+        System.out.println("-------------------");
+
+        //public int indexOf(String str,int fromIndex)：返回指定字符串在此字符串中从指定位置后第一次出现处的索引   自己练习
+
+        //public String substring(int beginIndex)：从指定位置开始截取字符串，默认到末尾
+        System.out.println("substring:"+s.substring(5));
+        System.out.println("substring:"+s.substring(0));
+        System.out.println("-------------------");
+        //public String substring(int beginIndex,int endIndex)：从指定位置开始到指定位置结束截取字符串。
+        System.out.println("substring:"+s.substring(1,5));
     }
 }
