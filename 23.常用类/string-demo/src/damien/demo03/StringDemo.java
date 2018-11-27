1308b19dccf934891d955919dcdcc7be15e3a10f
damienzhong <damienzhong@foxmail.com>
2018-11-27 14:32:39 +0800

    String 判断方法

---
+++ 23.常用类/string-demo/src/damien/demo03/StringDemo.java
@@ -0,0 +1,13 @@
+package damien.demo03;
+
+/**
+ * 判断功能：
+ *  public boolean equals(Object anObject)
+ *  public boolean equalsIgnoreCase(String anotherString)
+ *  public boolean contains(CharSequence s)
+ *  public boolean startsWith(String prefix)
+ *  public boolean endsWith(String suffix)
+ *  public boolean isEmpty()
+ */
+public class StringDemo {
+}
