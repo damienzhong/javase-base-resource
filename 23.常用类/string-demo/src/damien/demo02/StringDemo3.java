f76db1d3635478495ce413e46cf065dc38d9028c
damienzhong <damienzhong@foxmail.com>
2018-11-27 21:25:30 +0800

    String面试题

--- 23.常用类/string-demo/src/damien/demo02/StringDemo3.java
+++ 23.常用类/string-demo/src/damien/demo02/StringDemo3.java
@@ -7,17 +7,17 @@ public class StringDemo3 {
     public static void main(String[] args) {
         String s1 = new String("hello");
         String s2 = new String("hello");
-        System.out.println(s1==s2);
-        System.out.println(s1.equals(s2));
+        System.out.println(s1==s2);//false
+        System.out.println(s1.equals(s2));//true
 
         String s3 = new String("hello");
         String s4 = "hello";
-        System.out.println(s3==s4);
-        System.out.println(s3.equals(s4));
+        System.out.println(s3==s4);//false
+        System.out.println(s3.equals(s4));//true
 
         String s5 = "hello";
         String s6 = "hello";
-        System.out.println(s5==s6);
-        System.out.println(s5.equals(s6));
+        System.out.println(s5==s6);//true
+        System.out.println(s5.equals(s6));//true
     }
 }
