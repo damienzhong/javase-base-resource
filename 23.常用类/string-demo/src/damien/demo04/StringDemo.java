4e662e73c69177aab5e8dda8f6d3f2ed5dcb6a1f
damienzhong <damienzhong@foxmail.com>
2018-11-28 10:30:51 +0800

    String类的转换功能概述

---
+++ 23.常用类/string-demo/src/damien/demo04/StringDemo.java
@@ -0,0 +1,19 @@
+package damien.demo04;
+
+/**
+ *  String的转换功能：
+ *      public byte[] getBytes()：把字符串转换成字节数组
+ *      public char[] toCharArray()：将此字符串转换为一个新的字符数组。
+ *      public static String valueOf(char[] data)：把字符数组转成字符串
+ *      public static String valueOf(int i)：把int类型的数据转成字符串
+ *          注意：String类的valueOf方法可以把任意类型数据转成字符串
+ *      public String toLowerCase()：把字符串转成小写
+ *      public String toUpperCase(Locale locale)：把字符串转成大写
+ *      public String concat(String str)：把字符串拼接
+ *
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+
+    }
+}
