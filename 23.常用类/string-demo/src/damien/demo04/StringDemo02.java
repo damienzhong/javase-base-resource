9bdf36874354281810a3852df2d2c3bbc38873a1
damienzhong <damienzhong@foxmail.com>
2018-11-28 11:08:56 +0800

    String类的其他功能概述

---
+++ 23.常用类/string-demo/src/damien/demo04/StringDemo02.java
@@ -0,0 +1,21 @@
+package damien.demo04;
+
+/**
+ *  String类的其他功能：
+ *
+ *      替换功能：
+ *      public String replace(char oldChar,char newChar)
+ *      public String replace(CharSequence target,CharSequence replacement)
+ *
+ *      去除字符串两空格：
+ *      public String trim()
+ *
+ *      按字典顺序比较两个字符串：
+ *      public int compareTo(String anotherString)
+ *      public int compareToIgnoreCase(String str)
+ */
+public class StringDemo02 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ 23.常用类/string-demo/src/damien/demo04/StringDemo03.java
@@ -0,0 +1,13 @@
+package damien.demo04;
+
+/**
+ *  看程序写结果
+ */
+public class StringDemo03 {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hel";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s2.compareTo(s1));
+    }
+}
