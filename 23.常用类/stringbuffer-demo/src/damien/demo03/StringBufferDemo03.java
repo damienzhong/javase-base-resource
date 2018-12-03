d9f7e4d39903a13b125d4db99ef7b2fa307f1f8a
damienzhong <damienzhong@foxmail.com>
2018-12-03 15:42:08 +0800

    面试题

---
+++ 23.常用类/stringbuffer-demo/src/damien/demo03/StringBufferDemo03.java
@@ -0,0 +1,13 @@
+package damien.demo03;
+
+/**
+ *  面试题：
+ *      1、String、StringBuffer、StringBuilder的区别？
+ *
+ *      2、StringBuffer与数组的区别？
+ */
+public class StringBufferDemo03 {
+    public static void main(String[] args) {
+
+    }
+}
