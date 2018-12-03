7b8e7cf6631d7349f458657a2c41f0419e4ed114
damienzhong <damienzhong@foxmail.com>
2018-12-03 14:25:54 +0800

    StringBuffer的截取功能

---
+++ 23.常用类/stringbuffer-demo/src/damien/demo02/StringBufferDemo03.java
@@ -0,0 +1,12 @@
+package damien.demo02;
+
+/**
+ *  StringBuffer的截取功能：
+ *      public String substring(int start)
+ *      public String substring(int start,int end)
+ */
+public class StringBufferDemo03 {
+    public static void main(String[] args) {
+
+    }
+}
