a7260ca3698a396eb11c7fba5448813818b4ce18
damienzhong <damienzhong@foxmail.com>
2018-12-03 11:41:27 +0800

    StringBuffer replace

--- 23.常用类/stringbuffer-demo/src/damien/demo02/StringBufferDemo.java
+++ 23.常用类/stringbuffer-demo/src/damien/demo02/StringBufferDemo.java
@@ -2,7 +2,7 @@ package damien.demo02;
 
 /**
  *  StringBuffer的替换功能：
- *
+ *      public StringBuffer replace(int start,int end,String str)：从start开始到end用str替换
  */
 public class StringBufferDemo {
     public static void main(String[] args) {
