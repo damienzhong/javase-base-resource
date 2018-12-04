e122c40a6135837bb241881688dc717190858692
damienzhong <damienzhong@foxmail.com>
2018-12-04 22:30:26 +0800

    StringBuffer的截取功能

--- 23.常用类/stringbuffer-demo/src/damien/demo02/StringBufferDemo03.java
+++ 23.常用类/stringbuffer-demo/src/damien/demo02/StringBufferDemo03.java
@@ -1,12 +1,27 @@
 package damien.demo02;
 
 /**
- *  StringBuffer的截取功能：
- *      public String substring(int start)
- *      public String substring(int start,int end)
+ * StringBuffer的截取功能：注意：返回值类型不再是StringBuffer本身了
+ * public String substring(int start)
+ * public String substring(int start,int end)
  */
 public class StringBufferDemo03 {
     public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+
+        //添加数据
+        sb.append("hello").append("world").append("java");
+        System.out.println("sb:" + sb);
+
+        //public String substring(int start)
+        String s = sb.substring(5);
+        System.out.println("s:" + s);
+        System.out.println("sb:" + sb);
+
+        //public String substring(int start,int end)
+        String s2 = sb.substring(5,10);
+        System.out.println("s2:" + s2);
+        System.out.println("sb:" + sb);
 
     }
 }
