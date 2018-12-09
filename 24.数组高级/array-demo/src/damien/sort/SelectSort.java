d92b373848adf15fdb48f70843d5e91ee63a8f9f
damienzhong <damienzhong@foxmail.com>
2018-12-09 21:31:46 +0800

    选择排序

--- 24.数组高级/array-demo/src/damien/sort/SelectSort.java
+++ 24.数组高级/array-demo/src/damien/sort/SelectSort.java
@@ -8,5 +8,43 @@ package damien.sort;
 public class SelectSort {
     public static void main(String[] args) {
         int[] arr = {93, 21, 100, 76, 13};
+        System.out.println(printArray(arr));
+
+        System.out.println(printArray(selectSort(arr)));
+
+    }
+
+    /**
+     * 遍历数组
+     */
+    public static String printArray(int[] arr) {
+        String str = "[";
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                str += arr[i];
+            } else {
+                str = str + +arr[i] + ",";
+            }
+        }
+        str += "]";
+        return str;
+    }
+
+    /**
+     * 选择排序
+     * @param arr
+     * @return
+     */
+    public static int[] selectSort(int[] arr){
+        for (int x = 0; x < arr.length - 1; x++) {//控制的是排序的轮数
+            for (int y = x + 1; y < arr.length; y++) {
+                if (arr[x] > arr[y]) {
+                    int temp = arr[x];
+                    arr[x] = arr[y];
+                    arr[y] = temp;
+                }
+            }
+        }
+        return arr;
     }
 }
