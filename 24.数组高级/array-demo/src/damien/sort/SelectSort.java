7bc6786bd93129bcf5c74baf0aeb426c541ba99d
damienzhong <damienzhong@foxmail.com>
2018-12-09 16:02:07 +0800

    选择排序图解

---
+++ 24.数组高级/array-demo/src/damien/sort/SelectSort.java
@@ -0,0 +1,12 @@
+package damien.sort;
+
+/**
+ * 选择排序：
+ * 从0索引开始，依次和后面元素比较，小的往前放，第一次完毕，最小值出现在了最小索引处。
+ * 其他的同理，即可得到一个排好序的数组。
+ */
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] arr = {93, 21, 100, 76, 13};
+    }
+}
