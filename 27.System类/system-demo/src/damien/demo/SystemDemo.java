f642bf1daad8d81a914e3ea3c508224ec783f6db
damienzhong <damienzhong@foxmail.com>
2018-12-13 16:03:38 +0800

    add description about Class System

---
+++ 27.System类/system-demo/src/damien/demo/SystemDemo.java
@@ -0,0 +1,16 @@
+package damien.demo;
+
+/**
+ *  System类包含一些有用的类字段和方法。它不能被实例化。
+ *
+ *  方法：
+ *      public static void gc()
+ *      public static void exit(int status)
+ *      public static long currentTimeMillis()
+ *      public static void arraycopy(Object src,int srcPos,Object dest,int destPos,int length)
+ */
+public class SystemDemo {
+    public static void main(String[] args) {
+        
+    }
+}
