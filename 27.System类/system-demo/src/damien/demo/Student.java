ca630d7273b4c2127c91b71e96df276afa33483f
damienzhong <damienzhong@foxmail.com>
2018-12-13 21:45:36 +0800

    System.gc()

---
+++ 27.System类/system-demo/src/damien/demo/Student.java
@@ -0,0 +1,44 @@
+package damien.demo;
+
+/**
+ * @author damienzhong
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Student类执行finalize..."+this);
+        super.finalize();
+    }
+}
--- 27.System类/system-demo/src/damien/demo/SystemDemo.java
+++ 27.System类/system-demo/src/damien/demo/SystemDemo.java
@@ -4,12 +4,21 @@ package damien.demo;
  *  System类包含一些有用的类字段和方法。它不能被实例化。
  *
  *  方法：
- *      public static void gc()
+ *      public static void gc():运行垃圾回收器
  *      public static void exit(int status)
  *      public static long currentTimeMillis()
  */
 public class SystemDemo {
     public static void main(String[] args) {
+//        System.err.println("错误信息");
+//        System.out.println("普通信息");
+        Student student = new Student("呆萌钟", 25);
+        System.out.println(student);
+        Teacher teacher = new Teacher();
+        System.out.println(teacher);
 
+        student = null;
+        teacher = null;
+        System.gc();
     }
 }
