9f3ef07bccd70e0f9c28a1e982592423a719ecf5
damienzhong <damienzhong@foxmail.com>
2018-12-13 11:25:27 +0800

    regex demo

---
+++ 26.正则表达式/regex-demo/src/damien/demo01/RegexDemo.java
@@ -0,0 +1,13 @@
+package damien.demo01;
+
+/**
+ *  用代码实现如下功能：
+ *      输入任意字符串，校验该字符串是否属于手机号码。
+ *          校验规则：1、手机号码必须是11位数字
+ *                    2、0不能开头
+ */
+public class RegexDemo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 26.正则表达式/regex-demo/src/damien/demo01/RegexDemo2.java
@@ -0,0 +1,10 @@
+package damien.demo01;
+
+/**
+ *  正则表达式：是指一个用来描述或者匹配一系列符合某个语法规则的字符串的单个字符串。也可简称为符合一定规则的字符串。
+ */
+public class RegexDemo2 {
+    public static void main(String[] args) {
+
+    }
+}
