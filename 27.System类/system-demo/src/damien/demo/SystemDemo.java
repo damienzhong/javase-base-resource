1e0cd56278fb5ed9742dc7bc73b593a0477b606d
damienzhong <damienzhong@foxmail.com>
2018-12-13 16:54:52 +0800

    delete method arraycopy

--- 27.System类/system-demo/src/damien/demo/SystemDemo.java
+++ 27.System类/system-demo/src/damien/demo/SystemDemo.java
@@ -7,10 +7,9 @@ package damien.demo;
  *      public static void gc()
  *      public static void exit(int status)
  *      public static long currentTimeMillis()
- *      public static void arraycopy(Object src,int srcPos,Object dest,int destPos,int length)
  */
 public class SystemDemo {
     public static void main(String[] args) {
-        
+
     }
 }
