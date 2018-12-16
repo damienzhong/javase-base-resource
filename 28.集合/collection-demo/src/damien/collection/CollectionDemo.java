c3f8b3e5aa535ad29d0ca1bfd3509e4c6ad94cf3
damienzhong <damienzhong@foxmail.com>
2018-12-16 16:40:23 +0800

    Collection功能概述

--- 28.集合/collection-demo/src/damien/collection/CollectionDemo.java
+++ 28.集合/collection-demo/src/damien/collection/CollectionDemo.java
@@ -12,6 +12,24 @@ package damien.collection;
  *          数组可以存储基本数据类型，也可以存储引用数据类型
  *          集合只能存储引用类型
  *
+ *  Collection的功能概述：
+ *      1、添加功能：
+ *          boolean add(Object o)：添加一个元素
+ *          boolean addAll(Collection c):添加一个集合的元素
+ *      2、删除功能：
+ *          void clear()：从这个集合中移除所有的元素
+ *          boolean remove(Object o)：移除一个元素
+ *          boolean removeAll(Collection c)：删除此集合中包含的所有元素（可选操作）的所有元素（可选操作）
+ *      3、获取功能：
+ *          Iterator<E> iterator()：返回此集合中的元素的迭代器
+ *      4、判断功能：
+ *          boolean contains(Object o)：判断集合中是否包含指定的元素
+ *          boolean containsAll(Collection c)：判断集合中是否包含指定的集合元素
+ *          boolean isEmpty()：判断集合是否为空
+ *      5、长度功能
+ *          int size()：获取集合的长度
+ *      6、把集合转换为数组：
+ *          Object[] toArray()
  */
 public class CollectionDemo {
     public static void main(String[] args) {
