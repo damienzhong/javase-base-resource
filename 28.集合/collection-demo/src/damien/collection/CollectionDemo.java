ab38bcf5afde7addae6175e2a3469176cf72345f
damienzhong <damienzhong@foxmail.com>
2018-12-16 11:16:15 +0800

    集合框架概述

---
+++ 28.集合/collection-demo/src/damien/collection/CollectionDemo.java
@@ -0,0 +1,20 @@
+package damien.collection;
+
+/**
+ * 数组与集合的区别：
+ *      A:长度区别：
+ *          数组的长度是固定的
+ *          集合长度是可变的
+ *      B：内容不同
+ *          数组存储的是同一种类型的元素
+ *          集合可以存储不同类型的元素
+ *      C：存储的元素的数据类型不同
+ *          数组可以存储基本数据类型，也可以存储引用数据类型
+ *          集合只能存储引用类型
+ *
+ */
+public class CollectionDemo {
+    public static void main(String[] args) {
+
+    }
+}
