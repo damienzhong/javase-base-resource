9d7ec2be95a6dd85d281bba4addf4c5e8b8cfe7d
damienzhong <damienzhong@foxmail.com>
2018-12-19 22:14:11 +0800

    List概述

---
+++ 28.集合/collection-demo/src/damien/list/ListDemo2.java
@@ -0,0 +1,14 @@
+package damien.list;
+
+/**
+ *  List接口概述：
+ *      有序的Collection(也称为序列)。此接口的用户可以对列表中每个元素的插入位置进行精确地
+ *      控制。用户可以根据元素的整数索引（在列表中的位置）访问元素，并搜索列表中的元素。
+ *
+ *      与set不同，列表通常允许重复的元素。
+ */
+public class ListDemo2 {
+    public static void main(String[] args) {
+
+    }
+}
