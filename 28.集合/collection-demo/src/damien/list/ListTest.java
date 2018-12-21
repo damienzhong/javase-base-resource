48812a9ef497506e0a0e0a63163bc460230b9199
damienzhong <damienzhong@foxmail.com>
2018-12-21 23:09:38 +0800

    并发修改异常问题

---
+++ 28.集合/collection-demo/src/damien/list/ListTest.java
@@ -0,0 +1,19 @@
+package damien.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  题目：判断一个集合里面有没有"呆萌钟"这个元素，如果有，就添加一个元素"DM潮人社区",请写代码实现
+ */
+public class ListTest {
+    public static void main(String[] args) {
+        //创建List集合元素
+        List list = new ArrayList();
+
+        //添加元素
+        list.add("嗨~");
+        list.add("呆萌钟");
+        list.add("的淘宝店铺：");
+    }
+}
