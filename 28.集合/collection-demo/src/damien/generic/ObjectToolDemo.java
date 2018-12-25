fb481037146021773ad6f5071b44bd858e55e560
damienzhong <damienzhong@foxmail.com>
2018-12-25 22:43:38 +0800

    泛型类

--- 28.集合/collection-demo/src/damien/generic/ObjectToolDemo.java
+++ 28.集合/collection-demo/src/damien/generic/ObjectToolDemo.java
@@ -5,20 +5,31 @@ package damien.generic;
  */
 public class ObjectToolDemo {
     public static void main(String[] args) {
-        ObjectTool objectTool = new ObjectTool();
+//        ObjectTool objectTool = new ObjectTool();
+//
+//        objectTool.setObject(new Integer(25));
+//        Integer i = (Integer)objectTool.getObject();
+//        System.out.println("my age is " + i);
+//
+//        objectTool.setObject("呆萌钟");
+//        String s = (String)objectTool.getObject();
+//        System.out.println("my name is " + s);
+//
+//        System.out.println("--------------------------");
+//        objectTool.setObject(new Integer(100));
+//        //ClassCastException
+//        String i2 = (String)objectTool.getObject();
+//        System.out.println("my age is " + i2);
 
-        objectTool.setObject(new Integer(25));
-        Integer i = (Integer)objectTool.getObject();
-        System.out.println("my age is " + i);
+        ObjectTool<Integer> i = new ObjectTool<>();
+//        i.setObject("sfd");//编译不通过
+        i.setObject(20);
+        Integer age = i.getObject();
+        System.out.println("my age is " + age);
 
-        objectTool.setObject("呆萌钟");
-        String s = (String)objectTool.getObject();
-        System.out.println("my name is " + s);
-
-        System.out.println("--------------------------");
-        objectTool.setObject(new Integer(100));
-        //ClassCastException
-        String i2 = (String)objectTool.getObject();
-        System.out.println("my age is " + i2);
+        ObjectTool<String> s = new ObjectTool<>();
+        s.setObject("呆萌钟");
+        String name = s.getObject();
+        System.out.println("my age is " + name);
     }
 }
