a09ba81dc70c3af9501b06aed9fee47fcfae33f9
damienzhong <damienzhong@foxmail.com>
2018-12-27 22:51:30 +0800

    增强for循环

---
+++ 26.集合/jdk5-news/src/damien/news_01/ForDemo.java
@@ -0,0 +1,17 @@
+package damien.news_01;
+
+/**
+ *  JDK5新特性：
+ *      增强for循环：是for的一种
+ *
+ *  格式：
+ *      for(元素数据类型 遍历 : 数组或者Collection集合){
+ *          使用变量即可，该变量就是元素
+ *      }
+ *
+ */
+public class ForDemo {
+    public static void main(String[] args) {
+
+    }
+}
