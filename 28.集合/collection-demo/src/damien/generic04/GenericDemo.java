eb567e2433983580122272caa273ff97640d34a2
damienzhong <damienzhong@foxmail.com>
2018-12-27 22:24:19 +0800

    泛型通配符

---
+++ 28.集合/collection-demo/src/damien/generic04/GenericDemo.java
@@ -0,0 +1,25 @@
+package damien.generic04;
+
+/**
+ * 泛型：通配符
+ *  ?:
+ *  ? extends E:
+ *  ? super E:
+ */
+public class GenericDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Animal{
+
+}
+
+class Dog extends Animal{
+
+}
+
+class cat extends Animal{
+
+}
