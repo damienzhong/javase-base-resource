928577dc978089b684bd2b557dc69894e4ae96dc
damienzhong <damienzhong@foxmail.com>
2019-01-14 21:55:46 +0800

    TreeSet

---
+++ 28.集合/set-demo/src/damien/set/TreeSetDemo.java
@@ -0,0 +1,33 @@
+package damien.set;
+
+import java.util.TreeSet;
+
+/**
+ * TreeSet:由TreeMap实现，能够对元素按照某种规则进行排序
+ *  排序方式有两种：
+ *  1、自然排序
+ *  2、比较器排序
+ *
+ *  TreeSet集合的特点：排序和唯一
+ */
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        //添加元素
+        ts.add(6);
+        ts.add(2);
+        ts.add(3);
+        ts.add(1);
+        ts.add(4);
+        ts.add(6);
+        ts.add(1);
+        ts.add(5);
+        ts.add(2);
+        ts.add(7);
+
+        for (Integer i:ts){
+            System.out.println(i);
+        }
+    }
+}
