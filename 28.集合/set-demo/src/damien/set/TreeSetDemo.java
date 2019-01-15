641cfdcfaf51497f519a64d632da8434f0708092
damienzhong <damienzhong@foxmail.com>
2019-01-15 23:16:10 +0800

    TreeMap排序与唯一性图解

--- 28.集合/set-demo/src/damien/set/TreeSetDemo.java
+++ 28.集合/set-demo/src/damien/set/TreeSetDemo.java
@@ -12,6 +12,19 @@ import java.util.TreeSet;
  *  TreeSet集合的特点：排序和唯一
  *
  *  TreeSet底层是基于TreeMap实现的，其中，排序是通过Comparator实现的
+ *
+ *  TreeSet:底层是由二叉树结构实现的。（红黑树是一种自平衡的二叉树）
+ *
+ *  二叉树存储方式：
+ *      第一个元素的时候，直接作为根节点存储。
+ *      从第二个元素开始，每个元素从根节点开始找
+ *          大   就作为右子节点
+ *          小   就作为左子节点
+ *          相等  就不做存储
+ *
+ *  二叉树元素的获取：（前序遍历、中序遍历、后序遍历）
+ *      从跟节点开始，按照左、中右的原则依次取出元素
+ *
  */
 public class TreeSetDemo {
     public static void main(String[] args) {
