77ecfc168340d7e13f8708f935b99ce9e031ad4a
damienzhong <damienzhong@foxmail.com>
2019-01-15 23:26:38 +0800

    TreeMap排序与唯一性图解

---
+++ 28.集合/set-demo/src/damien/set/TreeSetDemo2.java
@@ -0,0 +1,12 @@
+package damien.set;
+
+/**
+ * TreeSet存储自定义学生对象并保证排序和唯一。
+ *  排序：自然排序，按照年龄从小到大排序
+ *  唯一：成员变量值都相同即为同一个元素
+ */
+public class TreeSetDemo2 {
+    public static void main(String[] args) {
+
+    }
+}
