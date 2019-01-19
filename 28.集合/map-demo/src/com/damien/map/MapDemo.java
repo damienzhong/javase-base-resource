dc9afa9b250282d81223aad02d0dba220d75388c
damienzhong <damienzhong@foxmail.com>
2019-01-19 11:44:42 +0800

    Map概述

---
+++ 28.集合/map-demo/src/com/damien/map/MapDemo.java
@@ -0,0 +1,14 @@
+package com.damien.map;
+
+/**
+ *  Map概述：
+ *      a:将键映射到值的对象。
+ *      b:一个映射不能包含重复的键。
+ *      c:每个键可以映射到至多一个值。
+ *  是存储的键值对
+ */
+public class MapDemo {
+    public static void main(String[] args) {
+
+    }
+}
