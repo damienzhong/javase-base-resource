d2f6f5feb150c0427565e990e6153ce39663a7cd
damienzhong <damienzhong@foxmail.com>
2019-01-20 22:34:33 +0800

    HashMap练习题

---
+++ 28.集合/map-demo/src/com/damien/hashmap/HashMapDemo3.java
@@ -0,0 +1,13 @@
+package com.damien.hashmap;
+
+/**
+ * HashMap<Student,String>
+ *     键：Student
+ *          要求：如果两个对象的成员变量值都相同，则为同一个对象
+ *     值：String
+ */
+public class HashMapDemo3 {
+    public static void main(String[] args) {
+
+    }
+}
