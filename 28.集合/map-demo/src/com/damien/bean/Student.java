3d5f6ac0829eb1b370c409bfff4aff66df3c506b
damienzhong <damienzhong@foxmail.com>
2019-01-20 22:55:11 +0800

    HashMap<Student,String>

--- 28.集合/map-demo/src/com/damien/bean/Student.java
+++ 28.集合/map-demo/src/com/damien/bean/Student.java
@@ -1,5 +1,7 @@
 package com.damien.bean;
 
+import java.util.Objects;
+
 /**
  * @author damienzhong
  */
@@ -35,5 +37,19 @@ public class Student {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return getAge() == student.getAge() &&
+                Objects.equals(getName(), student.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge());
     }
 }
--- 28.集合/map-demo/src/com/damien/hashmap/HashMapDemo3.java
+++ 28.集合/map-demo/src/com/damien/hashmap/HashMapDemo3.java
@@ -1,13 +1,37 @@
 package com.damien.hashmap;
 
+import com.damien.bean.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * HashMap<Student,String>
- *     键：Student
- *          要求：如果两个对象的成员变量值都相同，则为同一个对象
- *     值：String类型学号
+ * 键：Student
+ * 要求：如果两个对象的成员变量值都相同，则为同一个对象
+ * 值：String类型学号
  */
 public class HashMapDemo3 {
     public static void main(String[] args) {
+        HashMap<Student, String> hm = new HashMap<>();
 
+        //创建学生对象
+        Student s1 = new Student("超人", 55);
+        Student s2 = new Student("海王", 15);
+        Student s3 = new Student("神奇女侠", 63);
+        Student s4 = new Student("神奇女侠", 63);
+
+        //添加元素
+        hm.put(s1, "001");
+        hm.put(s2, "002");
+        hm.put(s3, "003");
+        hm.put(s4, "004");
+
+        //遍历
+        Set<Map.Entry<Student, String>> set = hm.entrySet();
+        for (Map.Entry<Student, String> me : set) {
+            System.out.println(me);
+        }
     }
 }
