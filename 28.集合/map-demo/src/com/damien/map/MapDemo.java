069f5c6ab005bd6717e8c65370cbd15630953992
damienzhong <damienzhong@foxmail.com>
2019-01-20 21:15:17 +0800

    Map's CUD

--- 28.集合/map-demo/src/com/damien/map/MapDemo.java
+++ 28.集合/map-demo/src/com/damien/map/MapDemo.java
@@ -1,5 +1,8 @@
 package com.damien.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *  Map概述：
  *      a:将键映射到值的对象。
@@ -29,6 +32,35 @@ package com.damien.map;
  */
 public class MapDemo {
     public static void main(String[] args) {
-
+        //创建集合对象
+        Map<String, String> map = new HashMap<>();
+        System.out.println("size:"+map.size());
+        System.out.println(map);
+        System.out.println("------------------");
+        //添加功能
+        //V put(K key, V value)：添加元素。重复的键，值会进行覆盖（可以理解为更新）
+        map.put("001","呆萌钟");
+        map.put("002","老付");
+        map.put("003","努力");
+        System.out.println("put:"+map.put("004","米豆"));
+        System.out.println("put:"+map.put("001","眨眼间"));
+        System.out.println(map);
+        System.out.println("------------------");
+        //删除功能
+        //void clear()：移除所有的键值对元素
+        //map.clear();
+        //  V remove(Object key)：根据键删除键值对元素，并把值返回
+//        System.out.println("remove:"+map.remove("001"));
+//        System.out.println(map);
+        //判断功能
+        //boolean containsKey(Object key)：判断集合是否包含指定的键
+//        System.out.println("containsKey:"+map.containsKey("0010"));
+        // boolean containsValue(Object value)：判断集合是否包含指定的值
+        //System.out.println("containsValue:"+map.containsValue("眨眼间"));
+        //boolean isEmpty()：判断集合是否为空
+        //System.out.println("isEmpty:"+map.isEmpty());
+        //长度功能
+        //int size()：返回集合中的键值对的对数
+        System.out.println("size:"+map.size());
     }
 }
