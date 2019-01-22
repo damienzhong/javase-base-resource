67cf8c5b02badc37c8f37f103f95e7a6f21745e3
damienzhong <damienzhong@foxmail.com>
2019-01-22 20:58:10 +0800

    TreeMap练习

---
+++ 28.集合/map-demo/src/com/damien/treemap/TreeMapDemo2.java
@@ -0,0 +1,12 @@
+package com.damien.treemap;
+
+/**
+ * TreeMap<Student,String>
+ *     键：Student
+ *     值：String
+ */
+public class TreeMapDemo2 {
+    public static void main(String[] args) {
+
+    }
+}
