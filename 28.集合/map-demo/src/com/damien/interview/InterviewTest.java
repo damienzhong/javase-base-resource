9212610eff85ea19c9e799963cbd60d365b0c8a1
damienzhong <damienzhong@foxmail.com>
2019-01-22 22:08:10 +0800

    面试题

---
+++ 28.集合/map-demo/src/com/damien/interview/InterviewTest.java
@@ -0,0 +1,13 @@
+package com.damien.interview;
+
+/**
+ * 面试题：
+ *  1、HashMap与Hashtable的区别？
+ *
+ *  2、List、Set、Map接口是否继承自Map接口？
+ */
+public class InterviewTest {
+    public static void main(String[] args) {
+
+    }
+}
