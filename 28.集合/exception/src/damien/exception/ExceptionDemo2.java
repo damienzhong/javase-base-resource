9318caf89a3495d0b00878e8bd5a9ebc98b7d73e
damienzhong <damienzhong@foxmail.com>
2019-01-24 23:01:44 +0800

    异常处理方式

---
+++ 28.集合/exception/src/damien/exception/ExceptionDemo2.java
@@ -0,0 +1,12 @@
+package damien.exception;
+
+/**
+ * 异常处理方式：
+ *  A：try...catch...finally
+ *  B：throws 抛出
+ */
+public class ExceptionDemo2 {
+    public static void main(String[] args) {
+
+    }
+}
