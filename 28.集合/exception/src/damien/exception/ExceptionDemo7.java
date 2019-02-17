dcd5506a3104985b9622989d3cf584b59d3cf3c7
damienzhong <damienzhong@foxmail.com>
2019-02-17 12:54:46 +0800

    throws

---
+++ 28.集合/exception/src/damien/exception/ExceptionDemo7.java
@@ -0,0 +1,12 @@
+package damien.exception;
+
+/**
+ * throws:
+ *  定义功能方法时，需要把出现的问题暴露出来，让调用者去处理。
+ *  那么就通过throws在方法上标识。
+ */
+public class ExceptionDemo7 {
+    public static void main(String[] args) {
+
+    }
+}
