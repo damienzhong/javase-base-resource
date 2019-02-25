1143a9498a9d22578ede7b3f5230a8177d3636a0
damienzhong <damienzhong@foxmail.com>
2019-02-25 21:12:21 +0800

    finally面试题

--- 28.集合/exception/src/damien/interview/FinallyTest.java
+++ 28.集合/exception/src/damien/interview/FinallyTest.java
@@ -3,6 +3,7 @@ package damien.interview;
 /**
  * 如果catch里面有return语句，请问finally里面的代码还会执行吗？
  * 如果会，请问是在return前还是return后？
+ *  会，前
  */
 public class FinallyTest {
     public static void main(String[] args) {
@@ -17,6 +18,11 @@ public class FinallyTest {
         } catch (ArithmeticException e) {
             a = 30;
             return a;
+            /*
+             * return a在程序执行到这一步的时候，这里不是return a而是return 30;这个返回路径就形成了，
+             * 但是呢，它发现后面还有finally，所以继续执行finally的内容，a=40;
+             * 再次回到以前的返回路径，继续走retrun 30;
+             */
         } finally {
             a = 40;
         }
